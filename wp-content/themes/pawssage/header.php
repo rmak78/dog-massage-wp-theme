6cd5e017c26b40f7d2d4a0b4a914bccc9b55b068
Zahid Badar <zahid@sutlej.net>
2015-06-18 01:47:11 +0500

    created top_ nav.php
    top nav

--- wp-content/themes/pawssage/header.php
+++ wp-content/themes/pawssage/header.php
@@ -99,16 +99,7 @@
             
             
             <div id="navbar" class="navbar-collapse collapse">
-              <ul class="nav navbar-nav top_ul">
-                <li class="active"><a href="index.html">Home</a></li>
-                <li><a href="#">About Us</a></li>
-                <li><a href="#">Benefits</a></li>
-                <li><a href="#">Services</a></li>
-                <li><a href="#">Faces</a></li>
-                <li><a href="#">Event Calendar</a></li>
-                <li><a href="#">FAQ</a></li>
-                <li><a href="#">Contact</a></li>
-              </ul> 
+              <?php include (TEMPLATEPATH . '/top_nav.php'); ?>
              </div> 
             </div> 
             </div><!--md-7--> 
