eaae48ce6c7f1030c8d0494b6462cf0b53e0aaa4
Waseem Mehmood <waseem@sutlej.net>
2015-06-18 00:50:39 +0500

    Static theme is ready

--- wp-content/themes/pawssage/footer.php
+++ wp-content/themes/pawssage/footer.php
@@ -1,3 +1,19 @@
-
+     <!-- FOOTER -->
+      <footer>
+        <div class="container">
+          <div class="row">
+            <div class="col-md-4 col-sm-4">
+              <p>&copy; 2009 - 2015 Pawssage.com </p> 
+            </div>  
+            <div class="col-md-6 col-sm-8 col-md-push-2">
+              <ul>
+                <li><a href="#">dogspaw Terms</a></li>
+                <li><a href="#">dogspaw Privacy</a></li>
+                <li><a href="#">dogspaw Contact</a></li>
+                <li><a href="#">dogspaw Bookings</a></li>
+              </ul>
+            </div>
+          </div> <!--row-->   
+       </div><!--container-->
 </body>
 </html>
