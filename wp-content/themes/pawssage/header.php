aa8fc3b5b2973854b7f318ad81c8494e99e114da
Waseem Mehmood <waseem@sutlej.net>
2015-06-18 00:49:33 +0500

    static theme is ready

--- wp-content/themes/pawssage/header.php
+++ wp-content/themes/pawssage/header.php
@@ -33,12 +33,194 @@
 	
 	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
 	
+	<link href="<?php echo get_stylesheet_directory_uri(); ?>/css/owl.carousel.css" rel="stylesheet">
+	
+	 <!-- Bootstrap core CSS -->
+    <link href="<?php echo get_stylesheet_directory_uri(); ?>/css/bootstrap.css" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+	
+    <link href="<?php echo get_stylesheet_directory_uri(); ?>/css/owl.theme.css" rel="stylesheet">
+	
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
 	<?php if ( is_singular() ) wp_enqueue_script('comment-reply'); ?>
 
-	<?php wp_head(); ?>
+	 <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/jquery.js"></script>
+	<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/bootstrap.js"></script>
+	<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/jquery-1.9.1.min.js"></script>
+	<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/owl.carousel.js"></script>
+	 <script>
+    $(document).ready(function() {
+ 
+     $("#owl-carousel").owlCarousel({
+        autoPlay: 3000, //Set AutoPlay to 3 seconds
+ 	    items : 5,
+        itemsDesktop : [1199,4],
+        itemsDesktopSmall : [979,3],
+
+  });
+ 
+});
+
+</script>
+	<?php //wp_head(); ?>
 </head>
 
-<body <?php body_class(); ?>>
+<body>
+
+ <div class="navbar-wrapper head">
+      <div class="container">
+            <div class="col-md-3 col-sm-3 col-xs-4 center">
+            <div class="navbar-header">
+             <a class="navbar-brand" href="index.html"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/11.png"></a>
+            </div>
+            </div><!--md-3-->
+            
+            <div class="col-md-7 col-sm-7 col-xs-12">
+             <div class="row margin-top">
+               <div class="col-md-9 col-sm-9 col-xs-8">
+                <h1>(209) 986 3516</h1>
+               </div>
+               <div class="col-md-3 col-sm-3 col-xs-4 top">
+                <a href="#" class="msg-btn">Book a Massage</a>
+               </div> 
+             </div>
+            <div class="row">
+ <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+            
+            
+            <div id="navbar" class="navbar-collapse collapse">
+              <ul class="nav navbar-nav top_ul">
+                <li class="active"><a href="index.html">Home</a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Benefits</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Faces</a></li>
+                <li><a href="#">Event Calendar</a></li>
+                <li><a href="#">FAQ</a></li>
+                <li><a href="#">Contact</a></li>
+              </ul> 
+             </div> 
+            </div> 
+            </div><!--md-7--> 
+           <div class="col-md-2 col-sm-2 col-xs-3 center mm">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/header-img_02.png">
+           </div>
+            
+      </div>
+    </div>
+<!-- carousel starts  -->    
+<div class="carousel-content">       
+  <div class="container"> 
+    <h1>Click on our faces!</h1> 
+      <p>we’re some of the canines that have benefited from the wonderful massage treatment by Pawssage!</p>     
+          <div id="owl-carousel" class="owl-carousel">
+			  <div class="item">
+                  <div class="custom">
+                    <a href="#"><img class="img-responsive" src="<?php echo get_stylesheet_directory_uri(); ?>/images/Pawssage1.png"></a>
+                     <div class="eclipse">
+                       <a href="#"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/hover_03.png"></a>
+                     </div>
+                  </div>
+                </div> 
+                
+                <div class="item">
+                  <div class="custom">
+                    <a href="#"><img class="img-responsive" src="<?php echo get_stylesheet_directory_uri(); ?>/images/Pawssage2.png"></a>
+                     <div class="eclipse">
+                       <a href="#"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/hover_03.png"></a>
+                     </div>
+                  </div>
+                </div>
+                
+                <div class="item">
+                  <div class="custom">
+                    <a href="#"><img class="img-responsive" src="<?php echo get_stylesheet_directory_uri(); ?>/images/Pawssage3.png"></a>
+                     <div class="eclipse">
+                       <a href="#"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/hover_03.png"></a>
+                     </div>
+                  </div>
+                </div>
+                
+                <div class="item">
+                  <div class="custom">
+                    <a href="#"><img class="img-responsive" src="<?php echo get_stylesheet_directory_uri(); ?>/images/Pawssage4.png"></a>
+                     <div class="eclipse">
+                       <a href="#"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/hover_03.png"></a>
+                     </div>
+                  </div>
+                </div>
+                
+                <div class="item">
+                  <div class="custom">
+                    <a href="#"><img class="img-responsive" src="<?php echo get_stylesheet_directory_uri(); ?>/images/Pawssage5.png"></a>
+                     <div class="eclipse">
+                       <a href="#"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/hover_03.png"></a>
+                     </div>
+                  </div>
+                </div>
+                
+                <div class="item">
+                  <div class="custom">
+                    <a href="#"><img class="img-responsive" src="<?php echo get_stylesheet_directory_uri(); ?>/images/Pawssage1.png"></a>
+                     <div class="eclipse">
+                       <a href="#"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/hover_03.png"></a>
+                     </div>
+                  </div>
+                </div>
+                
+                <div class="item">
+                  <div class="custom">
+                    <a href="#"><img class="img-responsive" src="<?php echo get_stylesheet_directory_uri(); ?>/images/Pawssage2.png"></a>
+                     <div class="eclipse">
+                       <a href="#"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/hover_03.png"></a>
+                     </div>
+                  </div>
+                </div>
+                
+                <div class="item">
+                  <div class="custom">
+                    <a href="#"><img class="img-responsive" src="<?php echo get_stylesheet_directory_uri(); ?>/images/Pawssage3.png"></a>
+                     <div class="eclipse">
+                       <a href="#"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/hover_03.png"></a>
+                     </div>
+                  </div>
+                </div> 
+                
+                <div class="item">
+                  <div class="custom">
+                    <a href="#"><img class="img-responsive" src="<?php echo get_stylesheet_directory_uri(); ?>/images/Pawssage4.png"></a>
+                     <div class="eclipse">
+                       <a href="#"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/hover_03.png"></a>
+                     </div>
+                  </div>
+                </div> 
+                
+                <div class="item">
+                  <div class="custom">
+                    <a href="#"><img class="img-responsive" src="<?php echo get_stylesheet_directory_uri(); ?>/images/Pawssage5.png"></a>
+                     <div class="eclipse">
+                       <a href="#"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/hover_03.png"></a>
+                     </div>
+                  </div>
+                </div> 
+                
+         </div> <!--carousel-->     
+	 </div><!--container --> 
+  </div><!--carousel-content-->         
+<!-- carousel ends  -->  	
  
+<div class="orange-bar">
+  <div class="container">
+    <p><span>Believe it or not:</span> UCD Professor finds <span class="yellow">sleeping</span> with your dog can be        <span class="grey">hazardous to your health.</span></p>
+  </div>
+</div><!--orange bar-->
--- wp-content/themes/pawssage/index.php
+++ wp-content/themes/pawssage/index.php
@@ -1,5 +1,48 @@
 <?php get_header(); ?>
  
+<div class="main-content">
+  <div class="container">
+    <div class="col-md-12">
+      <div class="row">
+        <div class="col-md-9">
+          <div class="main-content-top">
+            <h2>What is Canine massage</h2>
+              <p>Canine massage is based on the same principles of human massage. It is a therapeutic modality that can help relieve stress, relieve muscle tension, increase blood flow, and maintain the overall health of a dog. Massage is not a cure but a complementary treatment to be used in conjunction with regular veterinary care, chiropractic, acupuncture, proper conditioning, and nutrition.</p>
+              
+          </div><!--main content top-->
+          
+                   <div class="main-content-lower">
+                    <div class="row">
+                     <div class="col-md-8 col-sm-8">
+                       <h2>Who benefits?</h2>
+                         <p>All dogs can benefit from canine massage. Massage can help socialize 
+puppies and dogs that are not used to human touch. Older dogs benefit in a multitude of ways that can minimize the use of pain medication dependent on the conditions. The stress placed on the joints of overweight and dogs with bad structure can be minimized with a regime of massage, proper exercise, and veterinary care. Performance dogs train and compete in a way that has them engaging in repetitive movements that can create chronic problems due to the stress placed on their joints, muscles, tendons, and ligaments. Believe it or not, as you will see in Lotus’s case, active dogs that play hard with other dogs can develop soft tissue and joint problems that warrant both massage and chiropractic care.</p>
+                     </div><!--col-md-8-->
+                            <div class="col-md-4 col-sm-4 box-pad">
+                              <div class="box">
+                                <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/Pawssage_31.jpg">
+                              </div>
+                            </div>
+                    </div> <!--row--> 
+                   </div><!--main content lower-->
+        </div><!--col-md-9-->
+        
+          <div class="col-md-3 right">
+           <div class="widget">
+            <div class="widget-header">
+              <h3>Latest Dog Health News</h3>
+            </div>
+             <div class="widget-content">
+               <li><span class="orange-text">Winter Health:</span> This winter has been cold and damp. Everyone realizes that an older dog or an outdoor dog needs a coat to stay warm. <span class="orange-text">However, we must remember that if a dog has low body fat, health or injury issues, or is older, then that dog needs a jacket or coat also.</span></li>
+               <li><span class="orange-text">PAWS</span> need protecting in snow, ice, and salty conditions. This <span class="blue-text">article</span> discusses how to get your dog use to the <span class="blue-text">booties</span> (where to buy).</li>
+             </div> <!--widget news content-->
+            </div> <!--widget--> 
+          </div> <!--col-md-3-->
+      </div><!--row contains whole main content-->
+    </div><!--col-md-12 placed to adjust the row margin-->
+  </div><!--container-->
+</div><!--main content-->  
+ 
 <?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
