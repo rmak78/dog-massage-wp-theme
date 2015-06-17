40e5cfc5a66f2c02016782069053ffb2d9b79504
Rana Mansoor Akbar Khan <mansoor@sutlej.net>
2015-06-18 03:15:51 +0500

    Added Theme Support

--- wp-content/themes/pawssage/functions.php
+++ wp-content/themes/pawssage/functions.php
@@ -11,21 +11,62 @@ function custom_navigation_menus() {
 
 // Hook into the 'init' action
 add_action( 'init', 'custom_navigation_menus' );
+ 
 
-
+// Set content width value based on the theme's design
+if ( ! isset( $content_width ) )
+	$content_width = 700;
 
 // Register Theme Features
 function custom_theme_features()  {
 
-	// Add theme support for Featured Images
-	add_theme_support( 'post-thumbnails' );
+	// Add theme support for Automatic Feed Links
+	add_theme_support( 'automatic-feed-links' );
+
+	// Add theme support for Post Formats
+	add_theme_support( 'post-formats', array( 'status', 'quote', 'gallery', 'image', 'video', 'audio', 'link', 'aside', 'chat' ) );
+
+	// Add theme support for Custom Background
+	$background_args = array(
+		'default-color'          => '',
+		'default-image'          => '',
+		'default-repeat'         => '',
+		'default-position-x'     => '',
+		'wp-head-callback'       => '',
+		'admin-head-callback'    => '',
+		'admin-preview-callback' => '',
+	);
+	add_theme_support( 'custom-background', $background_args );
+
+	// Add theme support for Custom Header
+	$header_args = array(
+		'default-image'          => '',
+		'width'                  => 0,
+		'height'                 => 0,
+		'flex-width'             => false,
+		'flex-height'            => false,
+		'uploads'                => true,
+		'random-default'         => false,
+		'header-text'            => false,
+		'default-text-color'     => '',
+		'wp-head-callback'       => '',
+		'admin-head-callback'    => '',
+		'admin-preview-callback' => '',
+	);
+	add_theme_support( 'custom-header', $header_args );
+
+	// Add theme support for HTML5 Semantic Markup
+	add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list', 'gallery', 'caption' ) );
+
+	// Add theme support for document Title tag
+	add_theme_support( 'title-tag' );
+
+	// Add theme support for custom CSS in the TinyMCE visual editor
+	add_editor_style();
 }
 
 // Hook into the 'after_setup_theme' action
 add_action( 'after_setup_theme', 'custom_theme_features' );
-
-
-
-
+ 
 
 ?>
