59c6dd8a04bbd43649178cf5adf3ea066eedc325
Waseem Mehmood <waseem@sutlej.net>
2015-06-18 23:49:06 +0500

    working on page contents
    Not displaying the page contents..

--- wp-content/themes/pawssage/page.php
+++ wp-content/themes/pawssage/page.php
@@ -1,29 +1,18 @@
 <?php get_header(); ?>
 
-	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-			
-		<div class="post" id="post-<?php the_ID(); ?>">
-
-			
-
-			<?php include (TEMPLATEPATH . '/inc/meta.php' ); ?>
-
-			<div class="entry">
-
-				<?php the_content(); ?>
-
-				<?php wp_link_pages(array('before' => 'Pages: ', 'next_or_number' => 'number')); ?>
-
-			</div>
-
-			
-
-		</div>
-		
-		<?php // comments_template(); ?>
-
-		<?php endwhile; endif; ?>
-
+	<?php if(have_posts()): while(have_posts()): the_post(); ?>
+	
+	<h1><?php the_title(); ?></h1>
+	
+	<div class="entry">
+	
+	<?php the_content(); ?>
+	
+	</div>
+	<?php endwhile; ?>
+	
+	<?php endif; ?>
+	
 <?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
