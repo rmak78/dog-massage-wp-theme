dfa3b0a976ca6aae8658bf6a3db198eeb65a90ce
Rana Mansoor Akbar Khan <mansoor@sutlej.net>
2015-06-19 02:05:00 +0500

    Fixed the Page.php

--- wp-content/themes/pawssage/page.php
+++ wp-content/themes/pawssage/page.php
@@ -1,5 +1,10 @@
 <?php get_header(); ?>
-
+<div class="main-content">
+  <div class="container">
+    <div class="col-md-12">
+      <div class="row">
+        <div class="col-md-9">
+          <div class="main-content-top">
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 			
 		<div class="post" id="post-<?php the_ID(); ?>">
@@ -16,14 +21,31 @@
 
 			</div>
 
-			
+	  </div><!--main content top-->		
 
 		</div>
-		
+		  <div class="main-content-lower">
+                    <div class="row">
+                     <div class="col-md-8 col-sm-8">
 		<?php // comments_template(); ?>
 
 		<?php endwhile; endif; ?>
 
-<?php get_sidebar(); ?>
+          
+                             </div><!--col-md-8-->
+                          
+                    </div> <!--row--> 
+                   </div><!--main content lower-->         
+                      
+
+        </div><!--col-md-9-->
+        
+ <?php get_sidebar(); ?>
+ 
+      </div><!--row contains whole main content-->
+    </div><!--col-md-12 placed to adjust the row margin-->
+  </div><!--container-->
+</div><!--main content-->  
+ 
 
 <?php get_footer(); ?>
