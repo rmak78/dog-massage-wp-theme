bda76ab82fd9a2248053e0e9c74812cf7929e423
Waseem Mehmood <waseem@sutlej.net>
2015-06-18 06:17:05 +0500

    working on custom theme dev

--- wp-content/themes/pawssage/sidebar.php
+++ wp-content/themes/pawssage/sidebar.php
@@ -9,12 +9,12 @@
 ?>
          <div class="col-md-3 right">
            <div class="widget">
-            <div class="widget-header">
-              <h3>Latest Dog Health News</h3>
-            </div>
-             <div class="widget-content">
-               <li><span class="orange-text">Winter Health:</span> This winter has been cold and damp. Everyone realizes that an older dog or an outdoor dog needs a coat to stay warm. <span class="orange-text">However, we must remember that if a dog has low body fat, health or injury issues, or is older, then that dog needs a jacket or coat also.</span></li>
-               <li><span class="orange-text">PAWS</span> need protecting in snow, ice, and salty conditions. This <span class="blue-text">article</span> discusses how to get your dog use to the <span class="blue-text">booties</span> (where to buy).</li>
-             </div> <!--widget news content-->
-            </div> <!--widget--> 
+
+		  <?php if(is_active_sidebar('right_sidebar')) : ?>
+		  <ul>
+		  <?php dynamic_sidebar( 'right_sidebar' ); ?>
+		  </ul>
+		  <?php endif; ?>
+		  
+		     </div> <!--widget--> 
           </div> <!--col-md-3-->
