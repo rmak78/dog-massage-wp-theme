a5ce8812671dad94d8f8182fe571057374aa5634
Waseem Mehmood <waseem@sutlej.net>
2015-07-03 01:50:56 +0500

    make dynamic footer with widgets

--- wp-content/themes/pawssage/footer.php
+++ wp-content/themes/pawssage/footer.php
@@ -1,37 +1,12 @@
-     <!-- FOOTER -->
+ 
+
+
       <footer>
         <div class="container">
-          <div class="row">
-
-            <div class="col-md-4 col-sm-4" >
-
-						
-              <p>&copy; 2009 - 2015 <?php echo get_bloginfo('site_title'); ?> </p> 
-            </div> 
-		<!-- sutlej Logo start  -->
-					<div class="w1-ttl">            
-					<ul class="ttl">
-					<li class="logo"><a href="http://www.sutlej.net/" target="_blank" rel="Sutlej Creation" style="margin-top: 14px;">Sutlej</a></li>
-					
-					</ul>
-       					 </div>
-			<!--   sutlej Logo End --> 
-            <div class="col-md-6 col-sm-8 col-md-push-2">
-			<?php
-
-$defaults = array(
-	'theme_location'  => 'footer',
-	'menu'            => 'footer-menu',
-	'container'       => false,
-	'echo'            => true,
-	'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>'
-);
-
-wp_nav_menu( $defaults );
-
-?>
-            </div>
-	
+          <div class="row-fluid">
+<div id="footer-widgets" class="full-container">
+		<?php dynamic_sidebar( 'sidebar-footer' ) ?>
+	</div><!-- #footer-widgets -->
           </div> <!--row-->   
        </div><!--container-->
 <?php wp_footer(); ?>       
